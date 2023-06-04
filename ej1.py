47a6fd11fd8da44891ed74ec6c3a357cdff924ab
Ignacio Schuemer <ischuemer@udesa.edu.ar>
2023-06-04 11:53:53 -0300

    varios cambios

--- ej1.py
+++ ej1.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import os
 from pylab import *
+
 #printear las Imagenes de matplot en formato latex article
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
@@ -53,16 +54,20 @@ def show_first_last_eigenvec(Mat, p, k, save = False):
     # Visualizar en forma matricial p×p las primeras y las últimas dimensiones (autovectores) de la descomposición
 
     #primeros autovectores
-    first = Mat[:,k]
+    first = Mat[:,k].reshape(p,p)
     #ultimos autovectores
-    last = Mat[:,-k]
+
+    last = Mat[:,-(k+1)].reshape(p,p)
 
     #mostrar los primeros k autovectores y los ultimos k autovectores
     fig, axs = plt.subplots(1,2)
-    axs[0].imshow(first.reshape(p,p), cmap='gray')
-    axs[0].set_title(f'primeros {str(k)} autovectores')
-    axs[1].imshow(last.reshape(p,p), cmap='gray')
-    axs[1].set_title(f'ultimos {str(k)} autovectores')
+    axs[0].imshow(first, cmap='gray')
+    axs[0].set_title(f'Autovector {str(k)}')
+    axs[1].imshow(last, cmap='gray')
+    axs[1].set_title(f'Autovector {str(Mat.shape[1]-k)}')
+    #colorbar
+    # fig.colorbar(axs[0].imshow(first, cmap='viridis'), ax=axs[0])
+    # fig.colorbar(axs[1].imshow(last, cmap='viridis'), ax=axs[1])
     plt.show()
     if save:
         plt.savefig('first_last_eigenvec.svg', format="svg")
@@ -80,11 +85,8 @@ def find_d(U, S, Vt, Imagen, error):
             break
     return d
 
-def main():
-    directorio = 'dataset_Imagenes'
-    matrices_Imagenes = create_matrix_data(directorio)
-
-    show_matrix(matrices_Imagenes, 'matriz de datos')
+def punto_1(matrices_Imagenes, p):
+    # show_matrix(matrices_Imagenes, 'matriz de datos')
     # Imprimir el número de imágenes procesadas
     # Obtener el tamaño de la matriz de una Imagen con el primer vector 
     p = np.sqrt(matrices_Imagenes[:,0].shape[0]).astype(int)
@@ -92,9 +94,120 @@ def main():
     U, S, Vt = np.linalg.svd(matrices_Imagenes, full_matrices=True)
     # Visualizar en forma matricial p×p las primeras y las últimas dimensiones (autovectores)\
     #tomamos 1 autovector
-    show_first_last_eigenvec(U, p, 1)
+    show_first_last_eigenvec(U, p, 0, save = True)
 
 
+def punto_1_2(matrices_Imagenes, p, k):
+    # Imagen = matrices_Imagenes[:,k].reshape(p,p)
+    # U, S, Vt = np.linalg.svd(Imagen, full_matrices=False)
+    # # Calcular la norma de Frobenius de la matriz de la Imagen
+    # norma_frobenius = np.linalg.norm(Imagen, ord='fro')
+
+    # d = find_d(U, S, Vt, Imagen, 0.05)
+    # # Calcular el error de la compresión
+    # Imagen_reconstruida = U[:, :d] @ np.diag(S[:d]) @ Vt[:d, :]
+    # error = np.linalg.norm(Imagen - Imagen_reconstruida, ord='fro') / norma_frobenius
+    # print(f'Error con d = {d}: {error:.3f}')
+
+    # #printear la Imagen inicial y la reconstruida en una misma figura con subplots, agregarle una leyenda
+    # fig, axs = plt.subplots(1, 2)
+    # axs[0].imshow(Imagen, cmap='gray')
+    # axs[0].set_title('Imagen original')
+    # axs[1].imshow(Imagen_reconstruida, cmap='gray', label='Imagen reconstruida')
+    # axs[1].set_title('Imagen reconstruida con d = ' + str(d))
+    # #guardar la figura como svg
+    # plt.savefig('Imagenes_reconstruidas.svg', format="svg")
+    # plt.show()
+
+    # #con el d hallado, comprimir otra Imagen cualquiera del conjunto y ver el error, img es el nuemro de la imagen 
+    # img = 5
+    # Imagen2 = matrices_Imagenes[:,img].reshape(p,p)
+
+    # # Calcular la descomposición en valores singulares
+    # U, S, Vt = np.linalg.svd(Imagen2, full_matrices=False)
+
+    # # Calcular la norma de Frobenius de la matriz de la Imagen
+    # norma_frobenius = np.linalg.norm(Imagen2, ord='fro')
+
+    # # Obtener la matriz de la Imagen reconstruida de 28x28
+    # Imagen_reconstruida = U[:, :d] @ np.diag(S[:d]) @ Vt[:d, :]
+
+    # # Calcular el error de la compresión
+    # error = np.linalg.norm(Imagen2 - Imagen_reconstruida, ord='fro') / norma_frobenius
+
+    # # Imprimir el error
+    # print(f'Error con d = {d}: {error:.3f}')
+
+    # #printear la Imagen inicial y la reconstruida
+    # fig, axs = plt.subplots(1, 2)
+    # axs[0].imshow(Imagen2, cmap='gray')
+    # axs[0].set_title('Imagen original')
+    # axs[1].imshow(Imagen_reconstruida, cmap='gray')
+    # axs[1].set_title('Imagen reconstruida con d = ' + str(d))
+    # plt.savefig('ImagenesErrorConDanterior.svg', format="svg")
+    # plt.show()
+
+    # Imagen = matrices_Imagenes[:,5].reshape(p,p)
+    # d = [1, 3, 5, 7, 9, 11, 13, 15]
+    # #en una misma figura con subplots, agregarle una leyenda
+    # fig, axs = plt.subplots(1, len(d))
+    # for i in range(len(d)):
+    #     # Calcular la descomposición en valores singulares
+    #     U, S, Vt = np.linalg.svd(Imagen, full_matrices=False)
+    #     # Obtener la matriz de la Imagen reconstruida de 28x28
+    #     Imagen_reconstruida = U[:, :d[i]] @ np.diag(S[:d[i]]) @ Vt[:d[i], :]
+    #     #printear la Imagen inicial y la reconstruida
+    #     axs[i].imshow(Imagen_reconstruida, cmap='gray')
+    #     axs[i].axis('off')
+    #     axs[i].set_title('d = ' + str(d[i]))
+    # plt.savefig('ImagenesReconstruidas.svg', format="svg")
+    # plt.show()
+
+    #printear el error de la compresión para cada d y para cada imagen motrar la curva
+    #Seleccionamos las imagenes a las que le vamos a calcular el error
+    img = [0, 2, 5, 6]
+
+    #primero mostramos en una figura las imagenes originales
+    fig, axs = plt.subplots(1, len(img))
+    for i in range(len(img)):
+        Imagen = matrices_Imagenes[:,img[i]].reshape(p,p)
+        axs[i].imshow(Imagen, cmap='gray')
+        axs[i].axis('off')
+        axs[i].set_title('Imagen ' + str(i+1))
+    plt.savefig('ImagenesOriginales.svg', format="svg")
+    plt.show()
+
+    #calculamos el error para cada imagen y para cada d
+    d = np.arange(1, 28, 1)
+    error = np.zeros((d.shape[0], len(img)))
+    for i in range(len(d)):
+        for j in range(len(img)):
+            Imagen = matrices_Imagenes[:,img[j]].reshape(p,p)
+            # Calcular la descomposición en valores singulares
+            U, S, Vt = np.linalg.svd(Imagen, full_matrices=False)
+            # Obtener la matriz de la Imagen reconstruida de 28x28
+            Imagen_reconstruida = U[:, :d[i]] @ np.diag(S[:d[i]]) @ Vt[:d[i], :]
+            # Calcular la norma de Frobenius de la matriz de la Imagen
+            norma_frobenius = np.linalg.norm(Imagen, ord='fro')
+            # Calcular el error de la compresión
+            error[i,j] = np.linalg.norm(Imagen - Imagen_reconstruida, ord='fro') / norma_frobenius
+    #plotear el error en un mismo grafico indicando la imagen en las leyendas
+    plt.title('Error de la compresión de cada imagen para d = 1, 2, ..., 28')
+    #printear la curva
+    plt.plot(d, error)
+    plt.xlabel('d')
+    plt.ylabel('Error')
+    plt.legend(['Imagen 1', 'Imagen 2', 'Imagen 3', 'Imagen 4'])
+    plt.savefig('ErrorCompresion.svg', format="svg")
+    plt.show()
+   
+
+def main():
+    directorio = 'dataset_Imagenes'
+    matrices_Imagenes = create_matrix_data(directorio)
+    p = np.sqrt(matrices_Imagenes[:,0].shape[0]).astype(int)
+    # punto_1(matrices_Imagenes, p)
+    
     """
     Dada una Imagen cualquiera del conjunto (por ejemplo la primera) encontrar d, el número mínimo
     de dimensiones a las que se puede reducir la dimensionalidad de su representación mediante valores
@@ -103,71 +216,8 @@ def main():
     cualquiera del conjunto?
     """
     # Obtener la primera Imagen
-    Imagen = matrices_Imagenes[:,6].reshape(p,p)
-    # Calcular la descomposición en valores singulares
-    U, S, Vt = np.linalg.svd(Imagen, full_matrices=False)
-
-    # Calcular la norma de Frobenius de la matriz de la Imagen
-    norma_frobenius = np.linalg.norm(Imagen, ord='fro')
-
-    d = find_d(U, S, Vt, Imagen, 0.05)
-    # Calcular el error de la compresión
-    Imagen_reconstruida = U[:, :d] @ np.diag(S[:d]) @ Vt[:d, :]
-    error = np.linalg.norm(Imagen - Imagen_reconstruida, ord='fro') / norma_frobenius
-    print(f'Error con d = {d}: {error:.3f}')
-
-    #printear la Imagen inicial y la reconstruida en una misma figura con subplots, agregarle una leyenda
-    fig, axs = plt.subplots(1, 2)
-    axs[0].imshow(Imagen, cmap='gray')
-    axs[0].set_title('Imagen original')
-    axs[1].imshow(Imagen_reconstruida, cmap='gray', label='Imagen reconstruida')
-    axs[1].set_title('Imagen reconstruida con d = ' + str(d))
-    #guardar la figura como svg
-    plt.savefig('Imagenes_reconstruidas.svg', format="svg")
-    plt.show()
-
-    #con el d hallado, comprimir otra Imagen cualquiera del conjunto y ver el error, img es el nuemro de la imagen 
-    img = 5
-    Imagen2 = matrices_Imagenes[:,img].reshape(p,p)
-
-    # Calcular la descomposición en valores singulares
-    U, S, Vt = np.linalg.svd(Imagen2, full_matrices=False)
-
-    # Calcular la norma de Frobenius de la matriz de la Imagen
-    norma_frobenius = np.linalg.norm(Imagen2, ord='fro')
-
-    # Obtener la matriz de la Imagen reconstruida de 28x28
-    Imagen_reconstruida = U[:, :d] @ np.diag(S[:d]) @ Vt[:d, :]
-
-    # Calcular el error de la compresión
-    error = np.linalg.norm(Imagen2 - Imagen_reconstruida, ord='fro') / norma_frobenius
-
-    # Imprimir el error
-    print(f'Error con d = {d}: {error:.3f}')
-
-    #printear la Imagen inicial y la reconstruida
-    fig, axs = plt.subplots(1, 2)
-    axs[0].imshow(Imagen2, cmap='gray')
-    axs[0].set_title('Imagen original')
-    axs[1].imshow(Imagen_reconstruida, cmap='gray')
-    axs[1].set_title('Imagen reconstruida con d = ' + str(d))
-    plt.savefig('ImagenesErrorConDanterior.svg', format="svg")
-    plt.show()
-
-    Imagen = matrices_Imagenes[:,5].reshape(p,p)
-    d = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-    #en una misma figura con subplots, agregarle una leyenda
-    fig, axs = plt.subplots(1, len(d))
-    for i in range(len(d)):
-        # Calcular la descomposición en valores singulares
-        U, S, Vt = np.linalg.svd(Imagen, full_matrices=False)
-        # Obtener la matriz de la Imagen reconstruida de 28x28
-        Imagen_reconstruida = U[:, :d[i]] @ np.diag(S[:d[i]]) @ Vt[:d[i], :]
-        #printear la Imagen inicial y la reconstruida
-        axs[i].imshow(Imagen_reconstruida, cmap='gray')
-        axs[i].axis('off')
-    plt.savefig('ImagenesReconstruidas.svg', format="svg")
-    plt.show()
+    NumImagen = 6
+    punto_1_2(matrices_Imagenes, p, NumImagen)
 
 
 
--- ej2_dbscan.py
+++ ej2_dbscan.py
@@ -65,50 +65,128 @@ def dbscan(X, epsilon, min_muestras):
 def main():
     # Cargar el dataset
     X_original = np.loadtxt('dataset_clusters.csv', delimiter=',')
-    X = PCA(X_original,2)
 
-    # Parámetros de DBSCAN
-    epsilon = 0.67
-    min_muestras = 10
+    # U,S,V = np.linalg.svd(X_original)
+    # X = V[0:2,:]@X_original.T
+    # #plot
+    # plt.scatter(X[0,:],X[1,:])
+    # plt.show()
+    # X = PCA(X_original,2)
 
-    # Realizar el clustering con DBSCAN
-    dbscan_data = dbscan(X, epsilon, min_muestras)
-    # Graficar los clusters
-    centroids = []
-    for i in range(1, np.max(dbscan_data)):
-        centroids.append(np.mean(X[dbscan_data == i+1], axis=0))
-    centroids = np.array(centroids)
+    # # Parámetros de DBSCAN
+    # epsilon = 0.67
+    # min_muestras = 10
 
-    show_clustering_data(X, dbscan_data, centroids, title='DBSCAN')
-    distance_clasificator = find_subsets(X, centroids)
-    show_clustering_data(X, distance_clasificator, centroids, title='Clasificador de datos por distancia a centroides')
+    # # Realizar el clustering con DBSCAN
+    # dbscan_data = dbscan(X, epsilon, min_muestras)
+    # # Graficar los clusters
+    # centroids = []
+    # for i in range(1, np.max(dbscan_data)):
+    #     centroids.append(np.mean(X[dbscan_data == i+1], axis=0))
+    # centroids = np.array(centroids)
+
+    # show_clustering_data(X, dbscan_data, centroids, title='DBSCAN')
+    # distance_clasificator = find_subsets(X, centroids)
+    # show_clustering_data(X, distance_clasificator, centroids, title='Clasificador de datos por distancia a centroides')
 
 
-    # Cargar el dataset (3 dimensiones)
-    X_3d = PCA(X_original, 3)
+    # # Cargar el dataset (3 dimensiones)
+    # X_3d = PCA(X_original, 3)
 
-    # Visualizar el dataset	(3 dimensiones)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X_3d[:, 0], X_3d[:, 1], X_3d[:, 2])
-    plt.show()
+    # # Visualizar el dataset	(3 dimensiones)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(X_3d[:, 0], X_3d[:, 1], X_3d[:, 2])
+    # plt.show()
 
-    # Parámetros de DBSCAN para 3 dimensiones
-    epsilon_3d = 1.04
-    min_muestras_3d = 10
+    # # Parámetros de DBSCAN para 3 dimensiones
+    # epsilon_3d = 1.04
+    # min_muestras_3d = 10
 
-    # Realizar el clustering con DBSCAN para 3 dimensiones
-    labels_3d = dbscan(X_3d, epsilon_3d, min_muestras_3d)
+    # # Realizar el clustering con DBSCAN para 3 dimensiones
+    # labels_3d = dbscan(X_3d, epsilon_3d, min_muestras_3d)
 
-    # Graficar los clusters
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X_3d[:, 0], X_3d[:, 1], X_3d[:, 2], c=labels_3d, cmap='viridis')
-    plt.title('Clustering $\epsilon = 1.04$ y min_muestras = 10')
-    plt.xlabel('Eje X') #IMPORTANTE VER QUE PONER EN LOS LABEL X E Y
-    plt.ylabel('Eje Y')
-    plt.show()
-    plt.show()
+    # # Graficar los clusters
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(X_3d[:, 0], X_3d[:, 1], X_3d[:, 2], c=labels_3d, cmap='viridis')
+    # plt.title('Clustering $\epsilon = 1.04$ y min_muestras = 10')
+    # plt.xlabel('Eje X') #IMPORTANTE VER QUE PONER EN LOS LABEL X E Y
+    # plt.ylabel('Eje Y')
+    # plt.show()
+    # plt.show()
+
+    # Parámetros de DBSCAN para 4 dimensiones, luego hacemos un histograma para ver la distribución de los clusters	
+    # epsilon_4d = 1.04
+    # min_muestras_4d = 10
+    # X_4d = PCA(X_original, 4)
+    # # Realizar el clustering con DBSCAN para 4 dimensiones
+    # labels_4d = dbscan(X_4d, epsilon_4d, min_muestras_4d)
+    # print(np.max(labels_4d))
+    # # Graficar los histogramas para poder visualisar aglomeramiento de datos, no podemos verlo en 4 dimensiones por eso hacemos un histograma
+    # #graficar los histogramas de los clusters en una misma imagen
+    # plt.figure()
+    # for i in range(1, np.max(labels_4d)):
+    #     plt.hist(X_4d[labels_4d == i+1, 0], bins=20, alpha=0.5)  
+    # plt.title('Histograma de la primera componente de los datos agrupados por clusters')
+    # plt.xlabel('Componente 1')
+    # plt.ylabel('Densidad de puntos')
+    # plt.show()
+
+    # #printear el histograma de la primer componente luego de hacer el pca sin la mascara
+    # plt.figure()
+    # plt.hist(X_4d[:, 0], bins=20)
+    # plt.title('Histograma de la primera componente de los datos')
+    # plt.show()
+
+    # num_max_clusters = np.max(labels_4d)
+    # fig, axs = plt.subplots(num_max_clusters-1, 1, sharex=True, sharey=True)
+    # for i in range(num_max_clusters-1):
+    #     axs[i].hist(X_4d[labels_4d == i+2, 0], bins=20)
+    #     axs[i].set_title('Cluster ' + str(i+2))
+    # plt.xlabel('Eje X')
+    # plt.ylabel('Eje Y')
+    # plt.show()
+
+    # # # Parámetros de DBSCAN para 5 dimensiones, luego hacemos un histograma para ver la distribución de los clusters
+    # epsilon_5d = 1.04
+    # min_muestras_5d = 10
+    # X_5d = PCA(X_original, 5)
+    # # Realizar el clustering con DBSCAN para 5 dimensiones
+    # labels_5d = dbscan(X_5d, epsilon_5d, min_muestras_5d)
+    # print(np.max(labels_5d))
+
+
+    # # Parámetros de DBSCAN para 20 dimensiones, luego hacemos un histograma para ver la distribución de los clusters	
+    # epsilon_20d = 1.
+    # min_muestras_20d = 10
+    # X_20d = PCA(X_original, 20)
+    # # Realizar el clustering con DBSCAN para 20 dimensiones
+    # labels_20d = dbscan(X_20d, epsilon_20d, min_muestras_20d)
+
+    #printear el histograma de la primer componente de la proyeccion en d = p luego de hacer el pca sin la mascara
+    # X_106d = PCA(X_original, 106)
+    # plt.figure()
+    # plt.hist(X_106d[:, 0], bins=20)
+    # plt.title('Histograma de la primera componente de los datos')
+    # plt.show()
+
+    # # Graficar los histogramas para poder visualisar aglomeramiento de datos, no podemos verlo en 20 dimensiones por eso hacemos un histograma
+    # num_max_clusters = np.max(labels_20d)
+    # print(num_max_clusters)
+    # if num_max_clusters > 1:
+    #     fig, axs = plt.subplots(num_max_clusters, 1, sharex=True, sharey=True)
+    #     for i in range(num_max_clusters):
+    #         axs[i].hist(X_20d[labels_20d == i+1, 0], bins=20)
+    #         axs[i].set_title('Cluster ' + str(i+1))
+    #     plt.xlabel('Eje X')
+    #     plt.ylabel('Eje Y')
+    #     plt.show()
+    # else:
+    #     print('No se encontraron clusters')
+    
+
+
 
 
 if __name__ == "__main__":
